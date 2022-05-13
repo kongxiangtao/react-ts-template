0266851f8dfb88052e6bdda9c0398f72c8c2a9ce
xiangtao.kong <xiangtao.kong@quanshi.com>
2022-05-13 15:45:06 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import 'animate.css';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="App-header">
+                Welcome to React
+            </div>
+        </div>
+    );
+}
+
+export default App;
